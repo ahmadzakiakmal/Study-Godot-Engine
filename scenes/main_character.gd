aa57e06d7ebd0bb640008e3b6eb410bee9be7a93
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-10-03 18:25:15 +0700

    feat: animated sprite

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -2,13 +2,20 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -700.0
+const JUMP_VELOCITY = -600.0
 
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "walking"
+	else:
+		sprite_2d.animation = "default"
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		sprite_2d.animation = "jumping"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -20,6 +27,10 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 20)
 
 	move_and_slide()
+
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
+		
